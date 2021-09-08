57b6b1c7ac7927e5fc8ac29f63e0c7b221f8a50e
jun wei <tjw1993@gmail.com>
2021-09-08 15:50:01 +0800

    form structure of CRUD complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # list all
+  get "tasks", to: "tasks#index"
+  # create
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  # list details of one
+  get "tasks/:id", to: "tasks#show", as: :task
+  # edit details
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  patch "tasks/:id", to: "tasks#update", as: :task_update
+  # delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
